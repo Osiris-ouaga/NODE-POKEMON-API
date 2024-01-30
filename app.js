8701ee700708fac9d4ee4a14076b153b812dd0d0
Prince Divin MACKPAYEN <54855716+Osiris-ouaga@users.noreply.github.com>
2024-01-30 14:39:17 +0100

    CRUD endpoint fixed

--- app.js
+++ app.js
@@ -2,75 +2,18 @@ const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
-const { Sequelize } = require('sequelize')
-const {success, getUniqueId} = require('./helper.js')
-let pokemons = require("./mock-pokemon")
+const Sequelize = require('./src/db/sequelize')
 
 const app = express()
 const port = 3000
-
-const sequelize = new Sequelize(
-    'pokedex',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mariadb',
-        dialectOptions: {
-            timezone: 'Etc/GMT-2'
-        },
-        logging: false
-    }
-)
-
-sequelize.authenticate()
-.then(_ => console.log('La connexion à la base de données a bien été établie'))
-.catch(error => console.error(`Impossible de se connecter à la base de données ${error}`))
-
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
     .use(bodyParser.json())
 
-app.get('/', (req,res) => res.send('Hello, Express!'))
+    Sequelize.initDb()
 
-app.get('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    const message ='Un pokémon a bien été trouvé.'
-    res.json(success(message, pokemon))
-})
-
-app.get("/api/pokemons", (req, res) =>{
-    const message ='La liste des pokémons a bien été récupérée.'
-    res.json(success(message, pokemons))
-})
-
-app.post('/api/pokemons',(req, res) =>{
-    const id= getUniqueId(pokemons)
-    const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
-    pokemons.push(pokemonCreated)
-    const message = `Le pokemon ${pokemonCreated.name} a bien été créé.`
-    res.json(success(message, pokemonCreated))
-})
-
-app.put('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const pokemonUpdated = {...req.body, id: id}
-    pokemons= pokemons.map(pokemon =>{
-        return pokemon.id === id ? pokemonUpdated : pokemon
-    })
-    const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié.`
-    res.json(success(message, pokemonUpdated))
-})
-
-app.delete('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
-    pokemons.filter(pokemon => pokemon.id !== id)
-    const message = `Le pokemon ${pokemonDeleted} a bien été supprimé.`
-    res.json(success(message, pokemonDeleted))
-})
+    require('./src/routes/findAllPokemons')(app)
 
 app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
