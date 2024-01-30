c92d6f20634c71df937797d5163c21e3e51f434c
Prince Divin MACKPAYEN <54855716+Osiris-ouaga@users.noreply.github.com>
2024-01-30 15:29:15 +0100

    Logger system added

--- app.js
+++ app.js
@@ -15,5 +15,15 @@ app
     Sequelize.initDb()
 
     require('./src/routes/findAllPokemons')(app)
+    require('./src/routes/findPokemonByPK')(app)
+    require('./src/routes/createPokemon')(app)
+    require('./src/routes/updatePokemon')(app)
+    require('./src/routes/deletePokemon')(app)
+
+    //Gestion des erreurs 404
+    app.use(({res}) => {
+        const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
+        res.status(404).json({message})
+    }) 
 
 app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
