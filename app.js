50b9907f5f3a9ee6b577877d3f87223e7d13800a
Prince Divin MACKPAYEN <54855716+Osiris-ouaga@users.noreply.github.com>
2024-01-30 12:47:11 +0100

    CRUD endpoint added

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
+const { Sequelize } = require('sequelize')
 const {success, getUniqueId} = require('./helper.js')
 let pokemons = require("./mock-pokemon")
 
 const app = express()
 const port = 3000
 
+const sequelize = new Sequelize(
+    'pokedex',
+    'root',
+    '',
+    {
+        host: 'localhost',
+        dialect: 'mariadb',
+        dialectOptions: {
+            timezone: 'Etc/GMT-2'
+        },
+        logging: false
+    }
+)
+
+sequelize.authenticate()
+.then(_ => console.log('La connexion à la base de données a bien été établie'))
+.catch(error => console.error(`Impossible de se connecter à la base de données ${error}`))
+
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev')) 
+    .use(morgan('dev'))
     .use(bodyParser.json())
 
 app.get('/', (req,res) => res.send('Hello, Express!'))
@@ -34,6 +53,24 @@ app.post('/api/pokemons',(req, res) =>{
     pokemons.push(pokemonCreated)
     const message = `Le pokemon ${pokemonCreated.name} a bien été créé.`
     res.json(success(message, pokemonCreated))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = {...req.body, id: id}
+    pokemons= pokemons.map(pokemon =>{
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+})
+
+app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokemon ${pokemonDeleted} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
